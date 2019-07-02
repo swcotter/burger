0e00676244e083a9ef5c120de7ab5752a7cce2da
Samuel Cotter <swcotter@comcast.net>
2019-07-01 18:58:03 -0700

    First Push

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO cats (name) VALUES ('Cheese');
+INSERT INTO cats (name) VALUES ('Bacon');
+INSERT INTO cats (name, sleepy) VALUES ('Spicy', true);
+INSERT INTO cats (name, sleepy) VALUES ('Lots of pickles', true);
+INSERT INTO cats (name, sleepy) VALUES ('Everything burger', true);
